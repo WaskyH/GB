1eef38b980990594395e2372e893693e0acf9173
Konstantin <79270460+WaskyH@users.noreply.github.com>
2021-12-12 11:29:12 +0300

    GB
    GB

---
+++ HeloWorld 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"What`s youe name? "); // Вывод приведствия сообшения
+
+var name = Console.ReadLine();
+
+Console.WriteLine($"Hello,{name}![{ DateTime.Now}]");
+
+
